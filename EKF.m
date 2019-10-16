16b88b27af4509e6d3468fc48c255e68668953e3
U-CB152-ROW6-1\Landon Willey <willeylandon@gmail.com>
2019-10-16 09:56:19 -0600

    Changed Jacobians for the unicycle motion model.

--- EKF.m
+++ EKF.m
@@ -10,13 +10,13 @@ classdef EKF < handle
         sig_b_h
         K_h
         dt
-        L
+        Lm
         Q
         alph
         t
     end
     methods
-        function self = EKF(mu0, sig0, dt, sig_r, sig_th, alph, L, N)
+        function self = EKF(mu0, sig0, dt, sig_r, sig_th, alph, Lm, N)
             self.mu = mu0;
             self.sig = sig0;
             self.mu_h = [self.mu, zeros(3,N-1)];
@@ -25,7 +25,7 @@ classdef EKF < handle
             self.sig_b_h = self.sig_h;
             self.K_h = zeros(3,2,N);
             self.dt = dt;
-            self.L = L;
+            self.Lm = Lm;
             self.Q = [sig_r^2, 0; 0, sig_th^2];
             self.alph = alph;
         end
@@ -35,27 +35,21 @@ classdef EKF < handle
             self.correct(z);
         end
         function self = predict(self,u)
+            % Unicycle model: [vt*cos(th); vt*sin(th); wt]
             th = self.mu(3);
             vt = u(1);
             wt = u(2);
-            G = [1, 0, -vt/wt*cos(th)+vt/wt*cos(th+wt*self.dt);
-                 0, 1, -vt/wt*sin(th)+vt/wt*sin(th+wt*self.dt);
-                 0, 0, 1];
-            V = [(-sin(th)+sin(th+wt*self.dt))/wt,...
-                 (vt*(sin(th)-sin(th+wt*self.dt)))/wt^2+...
-                 (vt*cos(th+wt*self.dt)*self.dt)/wt;
-                 (cos(th)-cos(th+wt*self.dt))/wt,...
-                 -(vt*(cos(th)-cos(th+wt*self.dt)))/wt^2+...
-                 (vt*sin(th+wt*self.dt)*self.dt)/wt;
-                  0, self.dt];
-            M = [self.alph(1)*vt^2+self.alph(2)*wt^2, 0;
-                  0, self.alph(3)*vt^2+self.alph(4)*wt^2];
+            G = eye(3);
+            G(1,3) = vt*sin(th);
+            G(2,3) = vt*cos(th);
+            V = [cos(th), 0;
+                 sin(th), 0;
+                 0, self.dt];
+            M = [self.alph(1)*vt^2, 0;
+                  0, self.alph(2)*wt^2];
               
             % Update estimates
-            self.mu_bar = self.mu +...
-                [-vt/wt*sin(th)+vt/wt*sin(th+wt*self.dt);
-                 vt/wt*cos(th)-vt/wt*cos(th+wt*self.dt);
-                 wt*self.dt];
+            self.mu_bar = self.mu + [vt*cos(th); vt*sin(th); wt*self.dt];
             self.sig_bar = G*self.sig*G' + V*M*V';
             
             % Update histories
@@ -64,9 +58,9 @@ classdef EKF < handle
         end
         function self = correct(self,z)
             pz = 1;
-            for i = 1:length(self.L)
-                mx = self.L(1,i);
-                my = self.L(2,i);
+            for i = 1:length(self.Lm)
+                mx = self.Lm(1,i);
+                my = self.Lm(2,i);
                 mbx = self.mu_bar(1);
                 mby = self.mu_bar(2);
                 mbth = self.mu_bar(3);
